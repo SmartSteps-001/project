a79862f46d74dca79dd95cc30617349680ed3b05
SmartSteps-001 <smartstepstutorial1@gmail.com>
2025-07-24 12:10:44 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import './style.css'
-import './advancedNoiseSuppression.js'
+
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
