b747f2451e909e27f8bc4d542e8b65d388a4c861
SmartSteps-001 <smartstepstutorial1@gmail.com>
2025-07-24 11:55:18 +0100

    Update config.js

--- modules/config.js
+++ modules/config.js
@@ -9,13 +9,16 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// Use MongoDB Atlas instead of localhost
+const MONGO_URI = 'mongodb+srv://faithabayomi18:f1vouroluw11972@dominionspecialist.cdp3oi9.mongodb.net/videocall?retryWrites=true&w=majority&appName=dominionspecialist';
+
 export const setupDatabase = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/videocall', {
+    await mongoose.connect(MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
-    console.log('Connected to MongoDB');
+    console.log('Connected to MongoDB Atlas');
   } catch (err) {
     console.error('MongoDB connection error:', err);
   }
